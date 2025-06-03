6a1131ec1caf904cf0cecdf5a312678031fb8d0a
Shalamin <johan.keraudren@etudiant.univ-rennes.fr>
2025-06-03 11:49:43 +0200

    :sparkless: ajout des fxml avec les fenetres.

---
+++ src/main/java/vues/FenAccueil.java
@@ -0,0 +1,36 @@
+package vues;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class FenAccueil extends Stage {
+
+    public FenAccueil() {
+        this.setTitle("EllaPadNom");
+        this.setResizable(false);
+        Scene laScene = new Scene(creerSceneGraph());
+        this.setScene(laScene);
+
+    }
+
+    private Pane creerSceneGraph() {
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/fxml/acceuil.fxml"));
+
+        Pane root = new Pane();
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        loader.getController();
+        return root;
+    }
+}
