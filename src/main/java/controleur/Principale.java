342077ee3ae70363d57ac9060d27058f32d431da
Noah-LeGoff <noahlegoff22970@gmail.com>
2025-06-04 11:39:53 +0200

    Fermer fenetre ok

--- src/main/java/controleur/Principale.java
+++ src/main/java/controleur/Principale.java
@@ -7,13 +7,19 @@ import vues.*;
 
 public class Principale extends Application{
     static public FenAccueil fenAccueil;
-    static public FenetreEvenement 		fenEvent;
-    static public FenetreAffectation 	fenAffect;
-
-
+    static public FenTaches fenTaches;
+    static public FenCrEvent fenCrEvent;
+    static public FenCrTaches fenCrTaches;
+    static public FenModEvent fenModEvent;
+    static public FenModTaches fenModTaches;
 
     public void start(Stage f) throws IOException {
         fenAccueil = new FenAccueil();
+        fenTaches = new FenTaches();
+        fenCrEvent = new FenCrEvent();
+        fenCrTaches = new FenCrTaches();
+        fenModEvent = new FenModEvent();
+        fenModTaches = new FenModTaches();
         fenAccueil.show();
 
     }
@@ -26,21 +32,12 @@ public class Principale extends Application{
     // Gestion des fenêtres (à compléter)
     ////////////////////////////////////////////
 
-    public static void fermerAppli() {
-        System.exit(0);
-    }
-    public static void ouvrirFenEvent() {
-        fenEvent.show();
-    }
-
-    public static void fermerFenEvent() {
-        fenEvent.close();
-    }
-    public static void ouvrirFenAffectation() {
-    }
-    public static void fermerConfirmation() {
-        fenConfirmation.close();
-    }
+    public static void fermerFenAccueil() {System.exit(0);}
+    public static void fermerFenTaches() {fenTaches.close();}
+    public static void fermerFenCrEvent() {fenCrEvent.close();}
+    public static void fermerFenCrTaches() {fenCrTaches.close();}
+    public static void fermerFenModEvent() {fenModEvent.close();}
+    public static void fermerFenModTaches() {fenModTaches.close();}
 
 
     // enregistrement d'une réservation (se contente de l'afficher dans la console)
