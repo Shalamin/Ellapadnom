58f48aa644bbf588f9c954a8ec8c0e4433a30127
Bernard MICHEL <bernardmicheel@gmail.com>
2025-06-06 11:39:51 +0200

    Rectifications IDs FXML cr Event + ajout initialize dans le ctrl créer event

--- src/main/java/vues/action/CtrlCrEvent.java
+++ src/main/java/vues/action/CtrlCrEvent.java
@@ -1,6 +1,8 @@
 package vues.action;
 
 import controleur.Principale;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -8,12 +10,24 @@ import javafx.scene.control.*;
 public class CtrlCrEvent {
 
     @FXML private Button bnAnnuler;
-    @FXML private Button bnOk;
+    @FXML private Button bnOK;
+    @FXML private TextField txtNom;
+    @FXML private ComboBox selectSalle;
+    @FXML private ComboBox selectEvent;
+    @FXML private DatePicker date;
 
     @FXML void valider(ActionEvent event) {
+
 
     }
     @FXML void annuler(ActionEvent event){
         Principale.fermerFenCrEvent();
     }
+
+    @FXML void initialize(){
+        BooleanBinding pasPret = Bindings.or(Bindings.or(txtNom.textProperty().isEmpty(), selectSalle.armedProperty().asString().isEmpty()), Bindings.or(selectEvent.armedProperty().asString().isEmpty(), date.armedProperty().asString().isEmpty()));
+        bnOK.disableProperty().bind(Bindings.when(pasPret).then(true).otherwise(false));
+
+    }
+
 }
